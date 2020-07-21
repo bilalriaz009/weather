cbae5130dedd678b77537d4953938701ef0e0332
Bilal Riaz <bilu.m030@gmail.com>
2020-07-21 18:30:47 +0500

    site ready

--- src/components/HeaderComponnet.js
+++ src/components/HeaderComponnet.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Navbar,
   NavbarBrand,
   NavbarText
 } from 'reactstrap';
 
-const HeaderComponent = (props) => {
+const HeaderComponent = () => {
   return (
     <div>
       <Navbar color="light" light  expand="md">
         <NavbarBrand>
           <img src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather03-512.png"
-               width="60" height="60" 
+               width="60" height="60" alt="logo"
           />
         </NavbarBrand>
         <NavbarText><b>Weather Point</b></NavbarText>
